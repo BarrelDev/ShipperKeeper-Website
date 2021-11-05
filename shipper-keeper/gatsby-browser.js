86548a580dd71c9432c5b7f485b1d0036c12e68b
Sowmya R <sowmyas@baasha.net>
2021-11-05 14:55:14 -0400

    Add files via upload

--- shipper-keeper/gatsby-browser.js
+++ shipper-keeper/gatsby-browser.js
@@ -1,3 +1,19 @@
 import "./src/styles/styles.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./src/styles/palette.css";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyBV6QrJMAhuiDD2DofZvOR4FAIE48jT6IQ",
+    authDomain: "shipperkeeper-7a75a.firebaseapp.com",
+    projectId: "shipperkeeper-7a75a",
+    storageBucket: "shipperkeeper-7a75a.appspot.com",
+    messagingSenderId: "315456486414",
+    appId: "1:315456486414:web:3afd5681599ed885bc105f",
+    measurementId: "G-KSK23DHXPC"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);  
