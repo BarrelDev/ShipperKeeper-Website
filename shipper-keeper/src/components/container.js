cbc4be690d28ea4bdbbb6b2a6c70c0c4712448d2
Sowmya R <sowmyas@baasha.net>
2021-11-23 22:55:07 -0500

    Update container.js

--- shipper-keeper/src/components/container.js
+++ shipper-keeper/src/components/container.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { isMobile } from "react-device-detect";
+
 import "./container.module.css";
-import { header, subtitle, container, joinContainer, buttonCenter, text,cardBody } from "./container.module.css";
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import { Row } from 'react-bootstrap'
+import {
+    header, subtitle, container, joinContainer, buttonCenter, text,
+    cardBody, containerBlock, containerImg, containerTextLeft
+} from "./container.module.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 
 const SectionContainer = (props) => {
@@ -13,17 +18,17 @@ const SectionContainer = (props) => {
     const children = props.children;
     const imgPos = props.imagePosition;
     let imagePos = (imgPos === undefined) || (['left', 'right'].indexOf(imgPos) === -1) ? undefined : imgPos;
-    if (imagePos === 'left') {
+    if (imagePos === 'left' && !isMobile) {
         return (
             <Card className={container} >
                 <Card.Body className={cardBody}>
                     <Row>
-                        <Col sm={6} ><img src={bannerImage} alt=""/></Col>
-                        <Col sm={6} >
+                        <Col xs={12} md={6} ><img src={bannerImage} alt="" /></Col>
+                        <Col xs={12} md={6} >
+                            <div className={containerBlock}>
                             <Card.Title className={header}>{bannerTitle}</Card.Title>
                             <Card.Text className={subtitle}>{bannerSubtitle}</Card.Text>
                             <Card.Text className={text} >{children}</Card.Text>
-                            <div className={buttonCenter}> &nbsp;
                             </div>
                         </Col>
                     </Row>
@@ -31,26 +36,30 @@ const SectionContainer = (props) => {
                 </Card.Body>
             </Card>
         )
-    } else if (imagePos === 'right') {
+    } else if (imagePos === 'right' || isMobile) {
         return (
             <Card className={container}>
-                <Card.Body  className={cardBody}>
+                <Card.Body className={cardBody}>
                     <Row>
-                        <Col sm={6} >
-                            <Card.Title className={header}>{bannerTitle}</Card.Title>
-                            <Card.Text className={subtitle}>{bannerSubtitle}</Card.Text>
-                            <Card.Text className={text} >{children}</Card.Text>
-                            <div className={buttonCenter}> &nbsp;
+                        <Col xs={12} md={6}>
+                            <div className={containerTextLeft}>
+                                <Card.Title className={header}>{bannerTitle}</Card.Title>
+                                <Card.Text className={subtitle}>{bannerSubtitle}</Card.Text>
+                                <Card.Text className={text} >{children}</Card.Text>
                             </div>
                         </Col>
-                        <Col sm={6} ><img src={bannerImage} alt="" /></Col>
+                        <Col xs={12} md={6}>
+                            <div className={containerImg}>
+                                <img src={bannerImage} alt="" />
+                            </div>
+                        </Col>
 
                     </Row>
 
                 </Card.Body>
             </Card>
         )
-    }else {
+    } else {
         return (
             <div></div>
         )
@@ -68,7 +77,7 @@ const JoinContainer = (props) => {
                 <Card.Body>
                     <Card.Title className={header}>{bannerTitle}</Card.Title>
                     <div className={buttonCenter}>
-                        <Button href="https://docs.google.com/forms/d/e/1FAIpQLSeoZryrnwLPaHDJF-eywlRHGK7vI7y-bJ4SJwMDIUtLrX9saQ/viewform"  size="lg" target="_blank">Join Us</Button>
+                        <Button href="https://docs.google.com/forms/d/e/1FAIpQLSeoZryrnwLPaHDJF-eywlRHGK7vI7y-bJ4SJwMDIUtLrX9saQ/viewform" size="lg" target="_blank">Join Us</Button>
                     </div>
                 </Card.Body>
             </Card.ImgOverlay>
